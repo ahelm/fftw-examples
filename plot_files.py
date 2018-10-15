921776e72f06f8ed54277d9a6ff527f6256fa23c
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2018-10-15 11:29:50 +0100

    added ploting interface

---
+++ plot_files.py
@@ -0,0 +1,55 @@
+import pathlib
+import click
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def load_file(fname, column=None):
+    path = pathlib.Path(fname)
+    if column:
+        if not column.split(",")[-1]:
+            raise ValueError("Field specification can't end on comma")
+        column = np.array(column.split(","), dtype=int)
+    if not path.is_file:
+        raise ValueError(f"Not a valid file - argument passed: {fname}")
+    return np.loadtxt(path, usecols=column)
+
+
+def plot_multi_data(data, symbol, label=""):
+    for i in range(data.shape[-1]):
+        plt.plot(data[:, i], marker=symbol, label=label + f" [{i+1}]")
+
+
+def plot_line_data(data, symbol, label=""):
+    plt.plot(data[:], marker=symbol, label=label)
+
+
+@click.command()
+@click.argument("fn", nargs=-1, metavar="file")
+@click.option("--symbol", nargs=1, default=None)
+@click.option("--columns", nargs=1, type=str, default=None)
+@click.option("--diff", nargs=1, type=str, default=None)
+@click.option("--log", default=False, is_flag=True)
+def main(fn, symbol, columns, diff, log):
+    for f in fn:
+        data = load_file(f, column=columns)
+
+        if diff:
+            diff_data = load_file(diff, column=columns)
+            data -= diff_data
+
+        if len(data.shape) > 1:
+            plot_multi_data(data, symbol, label=f)
+        else:
+            plot_line_data(data, symbol, label=f)
+
+    plt.legend()
+
+    if log:
+        plt.yscale("log")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
