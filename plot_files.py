ce640e0fe5de96b17ac45619f8491147d8161968
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2018-10-16 21:36:18 +0100

    updated visualization routine

--- plot_files.py
+++ plot_files.py
@@ -15,23 +15,29 @@ def load_file(fname, column=None):
     return np.loadtxt(path, usecols=column)
 
 
-def plot_multi_data(data, symbol, label=""):
+def plot_multi_data(data, symbol, linestyle, label=""):
     for i in range(data.shape[-1]):
-        plt.plot(data[:, i], marker=symbol, label=label + f" [{i+1}]")
+        plt.plot(
+            data[:, i],
+            marker=symbol,
+            linestyle=linestyle,
+            label=label + f" [{i+1}]",
+        )
 
 
-def plot_line_data(data, symbol, label=""):
-    plt.plot(data[:], marker=symbol, label=label)
+def plot_line_data(data, symbol, linestyle, label=""):
+    plt.plot(data[:], marker=symbol, linestyle=linestyle, label=label)
 
 
 @click.command()
 @click.argument("fn", nargs=-1, metavar="file")
-@click.option("--symbol", nargs=1, default=None)
+@click.option("--marker", nargs=1, default=None)
+@click.option("--linestyle", nargs=1, default=None)
 @click.option("--columns", nargs=1, type=str, default=None)
 @click.option("--diff", nargs=1, type=str, default=None)
 @click.option("--log", default=False, is_flag=True)
 @click.option("--title", nargs=1, default="", type=str)
-def main(fn, symbol, columns, diff, log, title):
+def main(fn, marker, linestyle, columns, diff, log, title):
     for f in fn:
         data = load_file(f, column=columns)
         flabel = f
@@ -42,9 +48,9 @@ def main(fn, symbol, columns, diff, log, title):
             flabel += " | " + diff
 
         if len(data.shape) > 1:
-            plot_multi_data(data, symbol, label=flabel)
+            plot_multi_data(data, marker, linestyle, label=flabel)
         else:
-            plot_line_data(data, symbol, label=flabel)
+            plot_line_data(data, marker, linestyle, label=flabel)
 
     plt.legend()
 
